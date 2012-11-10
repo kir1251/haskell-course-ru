aab02191807d22eebba965a85849a9900458aeb5
Kirill <kirill@kirill-pc.(none)>
2012-11-10 12:51:40 +0400

    Finished exam task 1

--- midexam/Task1.hs
+++ midexam/Task1.hs
@@ -1,24 +1,39 @@
 import System.Directory
 import Data.List
-import Prelude (Show, Read, error, not, (&&), String, Bool, IO(..), return)
+import Prelude (Show, Read, error, not, (&&), (||), String, Bool, IO(..), return, (==), print)
  
-checkDE :: String -> String -> Bool
+checkDE :: String -> String -> IO Bool
 checkDE dir1 dir2 = do
+    let d1 = dir1
+    let d2 = dir2
     b1 <- doesDirectoryExist dir1
-    b2 <- doesDirectoryExist dir2
-    (not (b1 && b2))
+    b2 <- doesDirectoryExist dir2 
+    return (not (b1 && b2))
     
+concatIO :: [IO [String]] -> IO [String]
+concatIO [] = do return []
+concatIO  (x:list1) = do
+    a1 <- x
+    a <- concatIO list1
+    return (a1 ++ a)
+    
+appendIO :: [String] -> IO [String] -> IO [String]
+appendIO xs1 xs2 = do
+    a2 <- xs2
+    return (xs1 ++ a2)
+
 
 compareDirs :: String -> String -> IO [String]
 compareDirs dir1 dir2 = do
-    let b = checkDE dir1 dir2
+    b <- checkDE dir1 dir2     
     ans <- if (b) then do
        return []
     else do 
-        l1 <- getDirectoryContents dir1
+        l1 <- getDirectoryContents dir1  
         l2 <- getDirectoryContents dir2        
-        let l = [] ++ (l1 \\ l2)
+        let l = (l1 \\ l2)
         let f = nub (l1 ++ l2)
-        let l = l ++ concat [ do; a <- compareDirs (dir1 ++ "/" ++ s) (dir2 ++ "/" ++ s); return a | s <- f ] 
-        return l 
+        l <- appendIO l (concatIO [ compareDirs (dir1 ++ "/" ++ s) (dir2 ++ "/" ++ s)
+                                   | s <- f, not (s == ".." || s == ".") ])
+	return l 
     return ans
